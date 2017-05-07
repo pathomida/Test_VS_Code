77efde63b33b0b1e5f764eb960ebff9d927495f1
pathomida <pathomida@gmail.com>
2017-05-07 23:17:40 +0800

    first version

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.Office.Interop.Excel;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void myHelloWorld()
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            myHelloWorld();
+            int i =0 ;
+            for (i=0; i < 10 ; i++)
+            {
+                Console.Beep(1000,100);
+            }
+        }
+    }
+}
